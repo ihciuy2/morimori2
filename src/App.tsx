d74dd3dd58d506f69fa8f9601861db586eb5fb0d
ihciuy2 <m.ihciuy2@gmail.com>
2025-06-09 19:06:20 +0900

    API設定とAmazon商品情報自動取得の削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ProductProvider } from './context/ProductContext';
 import Header from './components/Header';
-import KeepaSettings from './components/KeepaSettings';
-import ProductAutoFetch from './components/ProductAutoFetch';
 import MainLayout from './components/MainLayout';
 
 function App() {
@@ -10,10 +8,6 @@ function App() {
     <ProductProvider>
       <div className="min-h-screen bg-gray-50 flex flex-col">
         <Header />
-        <div className="container mx-auto px-4 py-6 space-y-6">
-          <KeepaSettings />
-          <ProductAutoFetch />
-        </div>
         <MainLayout />
         <footer className="bg-white py-4 border-t border-gray-200">
           <div className="container mx-auto px-4 text-center text-gray-500 text-sm">
